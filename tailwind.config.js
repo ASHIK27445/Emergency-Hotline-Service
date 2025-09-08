2e30bf13f2fa815a750ee76598a4b73b511a8e95
ASHIK27445 <mdashikulislama57@gmail.com>
2025-09-08 17:23:19 +0600

    adding font inter and robota and working on with card to ruduce font size for better looking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 // tailwind.config.js
-module.exports = {
-  content: ["./*.html"], // or wherever your files are
-  theme: {
-    extend: {
-      fontFamily: {
-        hind: ["Hind Madurai", "sans-serif"]
-      },
-    },
-  },
-  plugins: [],
-}
+// module.exports = {
+//   content: ["./*.html"], // or wherever your files are
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         hind: ["Hind Madurai", "sans-serif"]
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
